abdb609f351e2d0ababd5c33801f44dbda6aea86
Serhii Prostakov <prostakov93@gmail.com>
2013-02-13 17:43:53 +0200

    trying to organize rendering of table

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,4 +1,5 @@
 class SchedulesController < ApplicationController
+include SchedulesHelper
 
 	def new
 	end
@@ -13,36 +14,10 @@ class SchedulesController < ApplicationController
 	end 
 
 	def show
-		@week1blocks = { pos1: [], pos2: [], pos3: [], pos4: [], pos5: [], pos6: []}
-		@week2blocks = { pos1: [], pos2: [], pos3: [], pos4: [], pos5: [], pos6: []}
 		@schedule = Schedule.find(params[:id])
 		@items = @schedule.items
-		@items.each do |item|
-			if item.position == 1 && week1
-				@week1blocks.pos1.push item
-			elsif item.position == 2 && week1
-				@week1blocks.pos2.push item
-			elsif item.position == 3 && week1
-				@week1blocks.pos3.push item
-			elsif item.position == 4 && week1
-				@week1blocks.pos4.push item
-			elsif item.position == 5 && week1
-				@week1blocks.pos5.push item
-			elsif item.position == 6 && week1
-				@week1blocks.pos6.push item
-			elsif item.position == 1 && week2
-				@week2blocks.pos1.push item
-			elsif item.position == 2 && week2
-				@week2blocks.pos2.push item
-			elsif item.position == 3 && week2
-				@week2blocks.pos3.push item
-			elsif item.position == 4 && week2
-				@week2blocks.pos4.push item
-			elsif item.position == 5 && week2
-				@week2blocks.pos5.push item
-			elsif item.position == 6 && week2
-				@week2blocks.pos6.push item
-			end
-		end
+		@items = sort_positions @items
+		@items = sort_days @items
+		@items = sort_weeks @items
 	end
 end
--- app/helpers/schedules_helper.rb
+++ app/helpers/schedules_helper.rb
@@ -1,2 +1,57 @@
 module SchedulesHelper
+
+	def sort_positions items
+		@result = [nil, [], [], [], [], [], []]
+		items.each do |item|
+			if item.position == 1
+				@result[1].push item
+			elsif item.position == 2
+				@result[2].push item
+			elsif item.position == 3
+				@result[3].push item
+			elsif item.position == 4
+				@result[4].push item
+			elsif item.position == 5
+				@result[5].push item
+			elsif item.position == 6
+				@result[6].push item
+			end	
+		end
+		return @result
+	end
+
+	def sort_days items
+		@result = [nil, [nil,[],[],[],[],[],[]], [nil,[],[],[],[],[],[]], [nil,[],[],[],[],[],[]], [nil,[],[],[],[],[],[]], [nil,[],[],[],[],[],[]], [nil,[],[],[],[],[],[]]]
+		(1..6).each do |k|
+			items[k].each do |item|
+				if item.day == 1
+					@result[k][1].push item
+				elsif item.day == 2
+					@result[k][2].push item
+				elsif item.day == 3
+					@result[k][3].push item
+				elsif item.day == 4
+					@result[k][4].push item
+				elsif item.day == 5
+					@result[k][5].push item
+				elsif item.day == 6
+					@result[k][6].push item
+				end
+			end
+		end
+		return @result
+	end
+
+	def sort_weeks items
+		(1..6).each do |k|
+			(1..6).each do |l|
+				if items[k][l][0] && items[k][l][0].week2
+					t = items[k][l][0]
+					items[k][l][0] = items[k][l][1]
+					items[k][l][1] = t
+				end
+			end
+		end
+		return items
+	end
 end
