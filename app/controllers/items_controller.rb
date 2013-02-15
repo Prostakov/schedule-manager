12e371dd021e785741fa830b722150ab4822f863
Serhii Prostakov <prostakov93@gmail.com>
2013-02-15 19:58:40 +0200

    started fucking with javascript

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,27 @@
 class ItemsController < ApplicationController
+	before_filter :correct_user_create, only: :create
 
 	def create
+		@item = @schedule.items.create(params[:item])
+		respond_to do |format|
+			format.hmtl {redirect_to @schedule}
+			format.json { render json: @item.to_json }
+		end
 	end
 
 	def show
 	end
 
 	def destroy
+	end
+
+	private
+
+	def correct_user_create
+		@schedule = Schedule.find(params[:schedule])
+		@user = @schedule.user
+		unless  @user == current_user
+			redirect_to root_path
+		end
 	end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ActiveRecord::Base
   attr_accessible :day, :location, :name, :position, :schedule_id, :teacher, :week1, :week2
   belongs_to :schedule
+
+  validates :name, presence: true
 end
