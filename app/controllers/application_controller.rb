90e4e5d42975259c0b886be7362096e1a9929b07
Serhii Prostakov <prostakov93@gmail.com>
2013-03-04 12:29:07 +0200

    MONSTER FEATURE

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
-
-  def hack_password user counts
-  	while counts
-  	  k = STDIN.gets.chomp.split(' ')[0]
-  	  return "Fuck Yeah! The password is #{k}" if user.authenticate(k)
-  	  counts -= 1
-  	end
-  	"You\'ve lost! Congrats, sucker!"
+  
+  def render_404
+    raise ActionController::RoutingError.new('Not Found')
   end
+
 end
--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -37,8 +37,14 @@ class SchedulesController < ApplicationController
 	end 
 
 	def show
-#		@schedule = Schedule.find_by_slug(params[:id]) ? @schedule : redirect_to Schedule.find_by_id(params[:id])
-		@schedule = Schedule.find_by_slug(params[:id])
+		if Schedule.find_by_slug(params[:id])
+			@schedule = Schedule.find_by_slug(params[:id])
+		elsif Schedule.find_by_id(params[:id])
+		 	@schedule = Schedule.find_by_id(params[:id])
+		 	redirect_to @schedule
+		else
+			render_404
+		end
 		if @schedule.weeks_counter
 			now = Time.now.getlocal(@schedule.local_time).to_date
 			week = now.strftime("%W").to_i
