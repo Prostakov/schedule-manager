042847b5fd4caa3181e1e5dd3a3562bee22189f4
Serhii Prostakov <prostakov93@gmail.com>
2013-03-28 00:12:39 +0200

    started working on groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,36 @@
 class GroupsController < ApplicationController
+  before_filter :correct_user_create, only: [:create]
+  before_filter :correct_user, only: [:update, :destroy, :edit]
 
   def show
     @group = Group.find_by_slug(params[:id])
     @school = @group.school
     @user = @school.user
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  private
+
+  def correct_user
+  	@school = School.find_by_slug(params[:school_id])
+  	@group = Group.where("school_id = ? AND slug = ?",@school.id,params[:id]).first
+  	@user = @school.user
+  	redirect_to root_path unless @user == current_user
+  end
+
+  def correct_user_create
+  	@user = User.find(params[:user])
+  	redirect_to root_path unless @user == current_user
   end
 end
