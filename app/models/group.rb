9a7c5b977484356b8067ddd00b851f47e30929c3
Serhii Prostakov <prostakov93@gmail.com>
2013-03-30 23:00:29 +0200

    added slug uniqueness validation for Group

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,10 +4,18 @@ class Group < ActiveRecord::Base
   belongs_to :school
   has_many :schedules, dependent: :destroy
 
-  validates :name, presence: true, length: {minimum: 2, maximum: 40}, uniqueness: {case_sensitive: false}
+  before_save :slug_uniqueness_validation
+
+  validates :name, presence: true, length: {minimum: 2, maximum: 40}
   validates :slug, presence: true
 
   def to_param
     self.slug
+  end
+
+  private
+
+  def slug_uniqueness_validation
+  	return false unless Group.where(school_id: self.school.id, slug: self.slug).empty?
   end
 end
